e0bb05e079353a796bde231361ff744507ebc960
zmigsod <starzmisgod@gmail.com>
2017-11-01 19:15:31 +0800

    new

--- models/posts.go
+++ models/posts.go
@@ -20,6 +20,12 @@ func ArticleAll(page, pagesize int) (interface{}, error) {
 	return lists, nil
 }
 
+type Tag struct {
+	ID            int
+	category_name string
+	category_id   int
+}
+
 func GetArticleLists(page, pagesize int) (interface{}, error) {
 	rows, _ := dbConn.Query(
 		"select p.ID as id,p.post_title,p.comment_count,p.post_date,p.post_intro,tm.name as category_name,u.user_nicename as author from wps_posts as p left join wps_term_relationships as rs on rs.object_id = p.ID left join wps_term_taxonomy as t on t.term_taxonomy_id = rs.term_taxonomy_id left join wps_terms as tm on tm.`term_id` = t.term_id left join wps_users as u on p.post_author = u.ID where p.post_status=?  and t.taxonomy = ? order by ID DESC limit ? offset ?",
@@ -46,10 +52,20 @@ func GetArticleLists(page, pagesize int) (interface{}, error) {
 			tagIds += "," + strconv.FormatInt(v, 10)
 		}
 		tagRune := []rune(tagIds)
-		finnalTag := string(tagRune[1:])
-		tags, err := ArticleTagsLists(finnalTag)
+		finalTag := string(tagRune[1:])
+		tags, err := ArticleTagsLists(finalTag)
 		if err == nil {
-			fmt.Println(tags)
+			mTags, ok := tags.([]map[string]interface{})
+			if ok {
+				// var tagsBind map[int64]interface{}
+				for _, vs := range mTags {
+					fmt.Println(vs["ID"])
+					// tagsBind[vs["ID"]] = vs
+				}
+				// fmt.Println(tagsBind)
+			} else {
+				fmt.Println("error")
+			}
 		}
 	}
 	return lists, nil
@@ -75,7 +91,7 @@ func ArticleTags(articleId int) (interface{}, error) {
 }
 
 func ArticleTagsLists(articleIds string) (interface{}, error) {
-	sqlSte := fmt.Sprintf("select tm.name as category_name,t.term_taxonomy_id as category_id from wps_posts as p left join wps_term_relationships as rs on rs.object_id = p.ID left join wps_term_taxonomy as t on t.term_taxonomy_id = rs.term_taxonomy_id left join wps_terms as tm on tm.`term_id` = t.term_id where p.ID in (%s) and t.taxonomy ='%s'", articleIds, "post_tag")
+	sqlSte := fmt.Sprintf("select tm.name as category_name,t.term_taxonomy_id as category_id,p.ID from wps_posts as p left join wps_term_relationships as rs on rs.object_id = p.ID left join wps_term_taxonomy as t on t.term_taxonomy_id = rs.term_taxonomy_id left join wps_terms as tm on tm.`term_id` = t.term_id where p.ID in (%s) and t.taxonomy ='%s'", articleIds, "post_tag")
 	rows, _ := dbConn.Query(sqlSte)
 	return DBQueryRows(rows)
 }
