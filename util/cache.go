379f92de08784eceb8a2dd2765081c96d888eb9d
zmisgod <starzmisgod@gmail.com>
2017-11-14 00:12:44 +0800

    ll

--- util/cache.go
+++ util/cache.go
@@ -26,3 +26,26 @@ func (cache *MyCache) Get(key string) interface{} {
 	kv := elem.Value.(*keyValue)
 	return kv.value
 }
+
+func (cache *MyCache) Len() int {
+	return cache.itemList.Len()
+}
+
+func NewMyCache(maxSize int) *MyCache {
+	return &MyCache{
+		itemList: list.New(),
+		itemMap:  make(map[string]*list.Element),
+		maxSize:  maxSize,
+	}
+}
+
+func (cache *MyCache) ClearAll() error {
+	cache.lock.Lock()
+	defer cache.lock.Unlock()
+
+	for k, v := range cache.itemMap {
+		cache.itemList.Remove(v)
+		delete(cache.itemMap, k)
+	}
+	return nil
+}
