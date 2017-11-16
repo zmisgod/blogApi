76856b952cb464f0edec15ce6692e4e1b2b8a802
zmigsod <starzmisgod@gmail.com>
2017-11-16 16:51:40 +0800

    New

--- models/sphinx.go
+++ models/sphinx.go
@@ -1,7 +1,7 @@
 package models
 
 func SphinxSearch(keyword string, page, pageSize int) (interface{}, error) {
-	SphinxClient.SetLimits((page-1)*pageSize, pageSize, 10000000, 0)
+	SphinxClient.SetLimits((page-1)*pageSize, pageSize, 1000, 0)
 	// 查询，第一个参数是我们要查询的关键字，第二个是索引名称test1，第三个是备注
 	res, err := SphinxClient.Query(keyword, "main", "search article!")
 	if err != nil {
